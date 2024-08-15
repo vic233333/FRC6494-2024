d28794ad795b9a11cf5bad60aadd0f52cb437cb0
vic233333 <93087330+vic233333@users.noreply.github.com>
2024-08-15 19:15:45 +0800

    style: 适配新的结构

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,14 +4,14 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
     private final TalonFX leftMotor;
     private final TalonFX rightMotor;
-    private final DutyCycleOut leftout = new DutyCycleOut(1);
-    private final DutyCycleOut rightout = new DutyCycleOut(-1);
 
     public ShooterSubsystem() {
 
@@ -38,10 +38,17 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     public void setShooterSpeed() {
-        leftMotor.setControl(leftout);
-        rightMotor.setControl(rightout);
+        leftMotor.setControl(new DutyCycleOut(Constants.Shooter.leftMotorOutput));
+        rightMotor.setControl(new DutyCycleOut(Constants.Shooter.rightMotorOutput));
     }
 
+    public Command shotCommand() {
+        return Commands.sequence(
+            Commands.runOnce(this::Shoot),
+            Commands.waitSeconds(3),
+            Commands.runOnce(this::stop)
+        );
+    }
 
     public void stop() {
         leftMotor.setControl(new DutyCycleOut(0));
