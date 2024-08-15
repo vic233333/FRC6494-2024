73972e90d7117b0b2de11fbe755c7cfed6e2656d
vic233333 <93087330+vic233333@users.noreply.github.com>
2024-08-15 19:14:45 +0800

    style: 优化书写格式

--- src/main/java/frc/robot/subsystems/AlignToAprilTagCommand.java
+++ src/main/java/frc/robot/subsystems/AlignToAprilTagCommand.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.Constants;
 import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.VisionSubsystem;
 
@@ -10,22 +11,26 @@ public class AlignToAprilTagCommand extends Command {
     private final Swerve swerve;
     private final VisionSubsystem vision;
     private final PIDController rotationController;
+    private final PIDController distanceController;
 
     public AlignToAprilTagCommand(Swerve swerve, VisionSubsystem vision) {
         this.swerve = swerve;
         this.vision = vision;
         addRequirements(swerve, vision);
-
-        rotationController = new PIDController(0.1, 0, 0);
-        rotationController.setTolerance(2);
-        rotationController.enableContinuousInput(-180, 180);
+        rotationController = new PIDController(Constants.Swerve.headingKP, Constants.Swerve.headingKI, Constants.Swerve.headingKD);
+        rotationController.setTolerance(1);
+        rotationController.enableContinuousInput(-Math.PI, Math.PI);
+        
+        distanceController = new PIDController(Constants.AutoConstants.kP, Constants.AutoConstants.kI, Constants.AutoConstants.kD);
+        distanceController.setTolerance(0.05); // 5cm tolerance
     }
 
     @Override
     public void execute() {
-        if (vision.hasValidTarget()) {
-            double rotationSpeed = rotationController.calculate(vision.getEstimatedPose().getRotation().getDegrees(), 0);
-            swerve.drive(new Translation2d(0, 0), rotationSpeed, true, false);
+        if (vision.haveSpeakerTarget()) {
+            double rotationSpeed = rotationController.calculate(vision.angleError().getRadians(), 0);
+            double forwardSpeed = distanceController.calculate(vision.distanceToSpeakerFromCenter(), Constants.Shooter.optimalShootingDistance);
+            swerve.drive(new Translation2d(forwardSpeed, 0), rotationSpeed, true, false);
         } else {
             swerve.stop();
         }
@@ -33,7 +38,7 @@ public class AlignToAprilTagCommand extends Command {
 
     @Override
     public boolean isFinished() {
-        return rotationController.atSetpoint() && vision.hasValidTarget();
+        return (rotationController.atSetpoint() && distanceController.atSetpoint()) && vision.haveSpeakerTarget();
     }
 
     @Override
