0534ef665c19fec0997ce74dd67e21456a10ed75
redrainflys <sfy220071215@163.com>
2024-08-21 16:12:41 +0800

    Create TeleopIntaker.java

---
+++ src/main/java/frc/robot/commands/TeleopIntaker.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.*;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class TeleopIntaker extends Command {    
+    private IntakerSubsysem s_Intaker; 
+    private DoubleSupplier leftRaw;
+    private DoubleSupplier rightRaw;
+
+    public TeleopIntaker(IntakerSubsysem s_Intaker,DoubleSupplier leftRaw,DoubleSupplier rightRaw) {
+        this.s_Intaker = s_Intaker;
+        addRequirements(s_Intaker);
+
+        this.leftRaw = leftRaw;
+        this.rightRaw = rightRaw;
+    }
+
+    @Override
+    public void execute() {
+        if(leftRaw.getAsDouble() >0.3 && rightRaw.getAsDouble() <0.3) s_Intaker.setIntakerSpeed();
+        else if (leftRaw.getAsDouble() > 0.3 && rightRaw.getAsDouble() >0.3) s_Intaker.setIntakerSpeedF();
+        else s_Intaker.stop();
+    }
+}
