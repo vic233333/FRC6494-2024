7af1910f945d97f835a0c43de8ada228a2f84187
jp-frc <jp_frc@126.com>
2024-12-20 17:33:42 +0800

    Update AutonomousCommand.java

--- src/main/java/frc/robot/AutonomousCommand.java
+++ src/main/java/frc/robot/AutonomousCommand.java
@@ -5,12 +5,10 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.subsystems.Swerve;
-import frc.robot.subsystems.VisionSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
-import frc.robot.subsystems.AlignToAprilTagCommand;
 
 public class AutonomousCommand extends SequentialCommandGroup {
-    public AutonomousCommand(Swerve swerve, VisionSubsystem vision, ShooterSubsystem shooter) {
+    public AutonomousCommand(Swerve swerve, ShooterSubsystem shooter) {
         addCommands(
             // Step 1: Drive backwards for 2 meters
             new RunCommand(() -> 
@@ -23,9 +21,6 @@ public class AutonomousCommand extends SequentialCommandGroup {
                     .andThen(new InstantCommand(swerve::stop)),
             
             // Step 2 & 3: Find AprilTags 7 and 8, and move to the best shooting position
-            new AlignToAprilTagCommand(swerve, vision)
-            .withTimeout(5)
-            .andThen(new InstantCommand(swerve::stop)),
             
             // Step 4: Shoot
             shooter.shotCommand()
